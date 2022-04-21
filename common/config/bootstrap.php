1293e7e814c23c1060418f5369ae18a1d43f46a3
zrh <loedan@163.com>
2022-04-22 00:14:50 +0800

    添加笔记文件

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * 设置别名，别名通常以 @ 开头
+ */
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
