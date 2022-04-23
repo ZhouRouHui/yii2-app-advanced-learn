03709edc7840af796135be154064f1295f36f8cb
zrh <loedan@163.com>
2022-04-23 15:32:27 +0800

    文章列表完善，PostSearch 类的学习

--- backend/views/post/index.php
+++ backend/views/post/index.php
@@ -17,30 +17,101 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('新增文章', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php
+    // 搜索框另外一种展示方式，不在列表表格内
+    // echo $this->render('_search', ['model' => $searchModel]);
+    ?>
 
+    <!--
+    GridView 小部件
+        - 是 Yii 中功能最强大的小部件之一
+        - 非常适合用来快速建立系统的管理后台
+        - 用 dataProvider 键指定提供数据的数据提供者
+        - 用 filterModel 键指定一个能够提供搜索过滤功能的搜索模型类，如果没有设置这一项，列表中就不会有搜索选项
+        - 用 columns 键指定需要展示的列及其格式
+    -->
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            // 序号列：行号，从 1 开始并且自动增长
+            //['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            /**
+             * 以下是数据列，用于显示数据
+             *
+             * 数据列是经常需要调整的内容，以 title 列为例，这里虽然只是简单的写了一个 title，
+             * 但是这是一种简写，背后有很多内容。如果需要改写，看例子：
+             * [
+             *      'class' => DataColumn::className(),
+             *      'attribute' => 'title',
+             *      'format' => 'text',
+             *      'label' => '标题'
+             * ]
+             * 这样的一个数组是背后设定 title 这一列的内容，当然这还不是全部的内容，
+             * 只是大部分都有默认值，如果我们什么都不需要改动，则可以直接写一个 title 这样的简写方式。
+             *
+             * 一个数据列经常需要自定义的字段有以下几个
+             *  - attribute 指定需要展示的属性
+             *  - label 标签名
+             *  - value 值
+             *  - format 格式
+             *  - filter 自定义过滤条件的输入框
+             *  - contentOptions 设定数据列 HTML 属性
+             * 这些都可以在上面案例数组中进行定义调整
+             */
+            // 重写 id 的展示，设置固定宽度
+//            'id',
+            [
+                'attribute' => 'id',
+                'contentOptions' => ['width' => '30px'],
+],
+
             'title',
-            'content:ntext',
+            // 'content:ntext',
+
+            // 重写 author_id 的显示，展示作者的 nickname
+            // 使用新增的属性 authorName 进行查询
+            // 'author_id',
+            [
+                'attribute' => 'authorName',
+                'label' => '作者',
+                'value' => 'author.nickname',
+            ],
             'tags:ntext',
-            'status',
+
+            // 重写 status 的显示，展示文章状态的 name
+            // 'status',
+            [
+                'attribute' => 'status',
+                'value' => 'status0.name',
+                // 对过滤条件框的内容进行自定义设置
+                'filter' => \common\models\Poststatus::find()
+                    ->select(['name', 'id'])
+                    ->orderBy('position')
+                    ->indexBy('id')
+                    ->column(),
+
+            ],
+
             //'create_time:datetime',
-            //'update_time:datetime',
-            //'author_id',
+//            'update_time:datetime',
+            [
+                'attribute' => 'update_time',
+                'format' => ['date', 'php:Y-m-d H:i:s'],
+            ],
+
+            /**
+             * 操作按钮列，用于显示查看，更新，删除等操作
+             */
             [
                 'class' => ActionColumn::className(),
                 'urlCreator' => function ($action, \common\models\Post $model, $key, $index, $column) {
                     return Url::toRoute([$action, 'id' => $model->id]);
-                 }
+                }
             ],
         ],
     ]); ?>
--- common/models/PostSearch.php
+++ common/models/PostSearch.php
@@ -7,18 +7,32 @@ use yii\data\ActiveDataProvider;
 use common\models\Post;
 
 /**
+ * 用于实现模型搜索功能
  * PostSearch represents the model behind the search form of `common\models\Post`.
  */
 class PostSearch extends Post
 {
     /**
+     * 重写 attributes 方法，为模型类添加属性
+     * 这里添加一个 authorName 属性解决文章列表页可以通过作者姓名来查询文章
+     * @return array|string[]
+     */
+    public function attributes()
+    {
+        return array_merge(parent::attributes(), ['authorName']);
+    }
+
+    /**
+     * 重写父类的 rules 方法
+     * 父类中的 rules 定义的内容是为了模型创建和更新时进行数据校验的，
+     * 而这里的 rules 定义的内容是为了对搜索表单的内容进行数据校验，可能校验的规则不一样，所以需要重写
      * {@inheritdoc}
      */
     public function rules()
     {
         return [
             [['id', 'status', 'create_time', 'update_time', 'author_id'], 'integer'],
-            [['title', 'content', 'tags'], 'safe'],
+            [['title', 'content', 'tags', 'authorName'], 'safe'],
         ];
     }
 
@@ -46,12 +60,22 @@ class PostSearch extends Post
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            // 设置分页
+            'pagination' => ['pageSize' => 5],
+            // 设置排序
+            'sort' => [
+                'defaultOrder' => ['id' => SORT_DESC],
+                // 默认 sort 里面不指定 attributes 的话，页面表头每个字段都可以点击更改排序方式
+                // 如果设置 attributes 的话，表示显示指定，不在指定范围内的字段就无法点击更改排序
+//                'attributes' => ['id', 'title'],
+            ]
         ]);
 
         $this->load($params);
 
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
+            // 如果验证不通过，并且不希望返回数据，可以放开下面这句 where 条件，默认是屏蔽的
             // $query->where('0=1');
             return $dataProvider;
         }
@@ -68,6 +92,16 @@ class PostSearch extends Post
         $query->andFilterWhere(['like', 'title', $this->title])
             ->andFilterWhere(['like', 'content', $this->content])
             ->andFilterWhere(['like', 'tags', $this->tags]);
+
+        // 添加 authorName 查询，需要关联 adminuser 表进行查询
+        $query->join('inner join', 'Adminuser', 'post.author_id = Adminuser.id');
+        $query->andFilterWhere(['like', 'Adminuser.nickname', $this->authorName]);
+
+        // 给 authorName 添加点击排序功能
+        $dataProvider->sort->attributes['authorName'] = [
+            'asc' => ['Adminuser.nickname' => SORT_ASC],
+            'desc' => ['Adminuser.nickname' => SORT_DESC],
+        ];
 
         return $dataProvider;
     }
